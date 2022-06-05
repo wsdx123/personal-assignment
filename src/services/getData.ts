071a201c65613401797cdf715189ac399698b014
Jun Young <50202150+wsdx123@users.noreply.github.com>
2022-06-05 15:56:01 +0900

    Update getData.ts

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,7 +1,8 @@
 import { axios } from 'hooks/worker'
 
-// const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
-const URL = `/2.0/?method=track.search&api_key=${process.env.REACT_APP_MUSIC_API}&format=json`
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
+
+const URL = `{PROXY}/2.0/?method=track.search&api_key=${process.env.REACT_APP_MUSIC_API}&format=json`
 interface Params {
   track: string
 }
