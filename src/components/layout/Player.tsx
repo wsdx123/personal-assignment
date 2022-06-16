e7dc79cb1b7e43cf3a6ab957826d2b914ee4a785
Jun Young <50202150+wsdx123@users.noreply.github.com>
2022-06-16 18:27:05 +0900

    fix: change svg

--- src/components/layout/Player.tsx
+++ src/components/layout/Player.tsx
@@ -1,10 +1,16 @@
 import { useNavigate } from 'react-router-dom'
 
-import { NextIcon, PlayIcon, PlayListIcon, PrevIcon } from 'assets/svgs'
+import { NextIcon, PauseIcon, PlayIcon, PlayListIcon } from 'assets/svgs'
 import styles from './player.module.scss'
+import { useState } from 'react'
 
 const Player = () => {
   const navigate = useNavigate()
+  const [clicked, setClicked] = useState(false)
+
+  const handlePlay = () => {
+    setClicked((prev) => !prev)
+  }
 
   const handleNavigate = () => {
     navigate('/playlist')
@@ -12,18 +18,24 @@ const Player = () => {
   return (
     <div className={styles.playerBar}>
       <div className={styles.music}>
-        <button type='button' aria-label='playlist' onClick={handleNavigate}>
-          <PlayListIcon className={styles.listIcons} />
-        </button>
+        {/* <button type='button' aria-label='playlist' onClick={handleNavigate}> */}
+        <PlayListIcon className={styles.listIcons} />
+        {/* </button> */}
         <dl className={styles.songInfo}>
           <dt>노래제목</dt>
           <dd>가수</dd>
         </dl>
       </div>
       <div>
-        <PrevIcon className={styles.icons} />
-        <PlayIcon className={styles.icons} />
-        <NextIcon className={styles.icons} />
+        <button type='button' aria-label='svgbutton'>
+          <NextIcon className={styles.prev} />
+        </button>
+        <button onClick={handlePlay} type='button' aria-label='svgbutton'>
+          {clicked ? <PlayIcon className={styles.play} /> : <PauseIcon className={styles.pause} />}
+        </button>
+        <button type='button' aria-label='svgbutton'>
+          <NextIcon className={styles.next} />
+        </button>
       </div>
     </div>
   )
